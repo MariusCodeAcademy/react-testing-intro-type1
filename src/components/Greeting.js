14fb0351c093c022bbefba85bf70d13c1482fd10
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-12 08:59:11 +0200

    testuzd

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,13 @@
 function Greeting() {
+  // state = isLoggedIn => false
+  // <p>We now how to test now</p> == false
+  // <p>Do we really, really? </p> == true
   return (
     <>
       <h2>Hello World!</h2>
       <p>We now how to test now</p>
+      <p>Do we really, really? </p>
+      <button>Toggle</button>
     </>
   );
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,12 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('renders main title hello world', () => {
-  // Arrange
-  render(<Greeting />);
-  //Act
+describe('Greeting items on screen', () => {
+  test('renders main title hello world', () => {
+    // Arrange
+    render(<Greeting />);
+    //Act
 
-  //Assert
-  const headingEl = screen.getByText('Hello World', { exact: false });
-  expect(headingEl).toBeInTheDocument();
+    //Assert
+    const headingEl = screen.getByText('Hello World', { exact: false });
+    expect(headingEl).toBeInTheDocument();
+  });
+
+  test('renders part of paragraph "how to test"', () => {
+    // Arrange
+    render(<Greeting />);
+
+    //Assert
+    const partOfParag = screen.getByText(/how to test/i);
+    expect(partOfParag).toBeInTheDocument();
+  });
 });
