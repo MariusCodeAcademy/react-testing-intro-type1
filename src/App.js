324eaec4bae11ffb882f19582a53e12e179a0d93
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-12 10:35:58 +0200

    fetch prekes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Greeting from './components/Greeting';
+import Shop from './components/Shop';
 
 function App() {
   return (
     <div className='App'>
       <Greeting title='Testing 3000' />
+      <Shop />
     </div>
   );
 }
---
+++ src/components/Shop.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+const URL = 'https://fakestoreapi.com/products';
+
+function Shop() {
+  const [shopItems, setShopItems] = useState([]);
+  // Kai tik uzsikraus komponentas
+  // pasissiusti prekes is https://fakestoreapi.com/products
+  // sudeti jas i state,
+  // atvaizduoti state prekes dom
+  useEffect(() => {
+    // iife - (()=>{})()
+    (async () => {
+      const resp = await fetch(URL);
+      const dataArr = await resp.json();
+      console.log({ dataArr });
+    })();
+  }, []);
+
+  return (
+    <section>
+      <h2>Our shop</h2>
+      <ul>
+        {shopItems.map(({ id, title, price }) => (
+          <li key={id}>
+            {' '}
+            {title} ${price}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Shop;
